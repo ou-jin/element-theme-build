54bb4272cf7e16ed17fb552dac01e63cc842b38f
oujin <945538628@qq.com>
2019-04-09 15:02:33 +0800

    file commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var path = require('path')
+var gulp = require('gulp')
+var cleanCSS = require('gulp-clean-css');
+var cssWrap = require('gulp-css-wrap');
+
+var customThemeName='.custom-theme'
+
+gulp.task('css-wrap', function() {
+  return gulp.src( path.resolve('./theme/index.css'))
+    .pipe(cssWrap({selector:customThemeName}))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('move-font', function() {
+  return gulp.src(['./theme/fonts/**']).pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('default',['css-wrap','move-font']);
+
+
+
+
